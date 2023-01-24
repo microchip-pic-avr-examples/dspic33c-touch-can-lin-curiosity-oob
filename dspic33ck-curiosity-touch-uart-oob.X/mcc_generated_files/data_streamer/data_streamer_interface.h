e482cd73a472b1b49a2772d90381d4a7210c9347
I20885 <Poornachandra.Bhatta@microchip.com>
2023-01-24 23:47:13 +0530

    MCU16GITHUB-660: dspic33ck-curiosity-touch-uart-oob-demo

---
+++ dspic33ck-curiosity-touch-uart-oob.X/mcc_generated_files/data_streamer/data_streamer_interface.h
@@ -0,0 +1,61 @@
+/**
+ * DATASTREAMER Generated Driver Interface API Header File.
+ * 
+ * @file data_streamer_interface.h
+ * 
+ * @ingroup datastreamer
+ * 
+ * @brief This declares the interface contract between the Data Streamer driver and the selected UART peripheral.
+ * 
+ * @version Data Streamer Driver Version 1.2.0
+ */
+ /*
+© [2023] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef DATA_STREAMER_INTERFACE_H
+#define	DATA_STREAMER_INTERFACE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * @ingroup datastreamer
+ * @struct data_streamer_interface_t 
+ * @brief Structure containing the function pointers to the Data Streamer communications APIs.
+ */
+typedef struct
+{
+    void (*Write)(uint8_t);
+    bool(*IsTxReady)(void);
+    bool(*IsTxDone)(void);
+} data_streamer_interface_t;
+
+/**
+ * @ingroup datastreamer
+ * @struct DATA_STREAMER 
+ * @brief Abstraction interface for the UART peripheral.
+ */
+extern const data_streamer_interface_t DATA_STREAMER;
+
+#endif	/* DATA_STREAMER_INTERFACE_H */
+/**
+ End of File
+*/
---
+++ dspic33ck-curiosity-touch-uart-oob.X/mcc_generated_files/data_streamer/src/data_streamer_interface.c
@@ -0,0 +1,40 @@
+/**
+ * DATASTREAMER Generated Driver Interface Source API File.
+ * 
+ * @file data_streamer_interface.c
+ * 
+ * @ingroup datastreamer
+ * 
+ * @brief This contains the implementation of the interface contract between the Data Streamer driver and the selected UART instance.
+ * 
+ * @version Data Streamer Driver Version 1.2.0
+ */
+ /*
+© [2023] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#include "../data_streamer_interface.h"
+#include "../../uart/uart1.h"
+
+const data_streamer_interface_t  DATA_STREAMER = {
+    .Write = &UART1_Write,
+    .IsTxReady = &UART1_IsTxReady,
+    .IsTxDone = &UART1_IsTxDone,
+};
